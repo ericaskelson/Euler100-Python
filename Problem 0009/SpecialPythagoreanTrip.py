941470fc8768007ebae22e381d067e864abef452
Eric Askelson <eraskelson@gmail.com>
2022-06-01 22:23:52 -0500

    Add solution for problem 9

---
+++ Problem 0009/SpecialPythagoreanTrip.py
@@ -0,0 +1,19 @@
+# Need to find a, b, and c, such that:
+# a < b < c
+# and a**2 + b**2 = c**2
+# and a + b + c = 1000
+# As usual, let's try the obvious way first
+
+breakdance = False
+n = 1000
+
+for b in range(2,n):
+    if breakdance: break
+    for a in range(1,b+1):
+        if breakdance: break
+        c=n-a-b
+        if (a**2 + b**2 == c**2): 
+            print("Eureka! a =",a,"b =",b,"c =",c,"Then a+b+c = ",a+b+c,"and abc = ",a*b*c)
+            breakdance = True
+
+# Well, that was very slow. Still, looks like the right answer
